b0d83ab05f1a5fdea1a51ed2f517ccec86dad078
Richard Brooks <rich90usa@gmail.com>
2011-09-27 23:20:35 -0400

    Fleshed out driver to specifications.

--- LabWeek4/DriverChangeMaker.java
+++ LabWeek4/DriverChangeMaker.java
@@ -1,12 +1,47 @@
 public class DriverChangeMaker {
 
     public static void main(String[] args) {
-        ChangeMaker eightyNine = new ChangeMaker((float) .89);
+        ChangeMaker cm = new ChangeMaker((float) .99);
 
-        System.out.println("Quarters: " + eightyNine.getQuarters());
-        System.out.println("Dimes: " + eightyNine.getDimes());
-        System.out.println("Nickels: " + eightyNine.getNickels());
-        System.out.println("Pennies: " + eightyNine.getPennies());
+        System.out.println("Quarters: " + cm.getQuarters());
+        System.out.println("Dimes: " + cm.getDimes());
+        System.out.println("Nickels: " + cm.getNickels());
+        System.out.println("Pennies: " + cm.getPennies()+ "\n");
+        
+        cm = new ChangeMaker((float) .83);
+
+        System.out.println("Quarters: " + cm.getQuarters());
+        System.out.println("Dimes: " + cm.getDimes());
+        System.out.println("Nickels: " + cm.getNickels());
+        System.out.println("Pennies: " + cm.getPennies()+"\n");
+
+        cm = new ChangeMaker((float) .75);
+
+        System.out.println("Quarters: " + cm.getQuarters());
+        System.out.println("Dimes: " + cm.getDimes());
+        System.out.println("Nickels: " + cm.getNickels());
+        System.out.println("Pennies: " + cm.getPennies()+"\n");
+
+        cm = new ChangeMaker((float) .49);
+
+        System.out.println("Quarters: " + cm.getQuarters());
+        System.out.println("Dimes: " + cm.getDimes());
+        System.out.println("Nickels: " + cm.getNickels());
+        System.out.println("Pennies: " + cm.getPennies()+"\n");
+
+        cm = new ChangeMaker((float) .36);
+
+        System.out.println("Quarters: " + cm.getQuarters());
+        System.out.println("Dimes: " + cm.getDimes());
+        System.out.println("Nickels: " + cm.getNickels());
+        System.out.println("Pennies: " + cm.getPennies()+"\n");
+        
+        cm = new ChangeMaker((float) .15);
+
+        System.out.println("Quarters: " + cm.getQuarters());
+        System.out.println("Dimes: " + cm.getDimes());
+        System.out.println("Nickels: " + cm.getNickels());
+        System.out.println("Pennies: " + cm.getPennies()+"\n");
     }
 
 }    
