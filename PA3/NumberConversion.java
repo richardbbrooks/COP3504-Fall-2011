e62986812c006a51891e223b54e4d9fea35ba486
Richard Brooks <rich90usa@gmail.com>
2011-10-25 20:35:16 -0400

    Got the implementation working.

--- PA3/NumberConversion.java
+++ PA3/NumberConversion.java
@@ -1,8 +1,14 @@
 public class NumberConversion {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         
-        DecimalToNewBase ten = new DecimalToNewBase(10);
+        DecimalToNewBase ten = new DecimalToNewBase(255);
         System.out.println(ten.decToBaseTwo());
+        System.out.println(ten.decToBaseThree());
+        System.out.println(ten.decToBaseFive());
+        System.out.println(ten.decToBaseEight());
+        System.out.println(ten.decToBaseNine());
+        System.out.println(ten.decToBaseTwelve());
+        System.out.println(ten.decToBaseSixteen());
+        System.out.println(ten.DecToN(16));
     }
 }
